0d815f31a977c3cf385683d81b69df87fc45ef4b
thefoxes86 <nicola.volpi86@gmail.com>
2023-11-10 15:38:16 +0100

    Insert Favicon

--- components/meta.tsx
+++ components/meta.tsx
@@ -7,27 +7,26 @@ export default function Meta() {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="https://backend.dicasapucci.com/wp-content/uploads/2023/11/favicon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="https://backend.dicasapucci.com/wp-content/uploads/2023/11/favicon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="https://backend.dicasapucci.com/wp-content/uploads/2023/11/favicon.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
+
       <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
+        rel="shortcut icon"
+        href="https://backend.dicasapucci.com/wp-content/uploads/2023/11/favicon-1.png"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
